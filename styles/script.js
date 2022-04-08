0549d2589b92bf1a6c41e36b39af69b8139355c5
mvrsdev <93167052+mvrsdev@users.noreply.github.com>
2022-04-08 16:01:22 -0300

    fix aside behavior

--- styles/script.js
+++ styles/script.js
@@ -4,6 +4,7 @@ console.log("Script loaded successfully!");
 // Get drawer menu instance by its ID
 const drawerMenu = document.getElementById("drawer-menu");
 
+
 // Add "click" event listener on toggle button
 document.getElementById("toggle-button").addEventListener("click", () => {
   // Invert display value according it's current value (if "none", then "flex", and vice-versa)
